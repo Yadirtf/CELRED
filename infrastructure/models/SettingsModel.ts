d37724d2e864beb0191c41bda950994880037206
Yadirtf <yadirtf0@gmail.com>
2026-03-14 18:43:27 -0500

    Mejora en mensaje

--- infrastructure/models/SettingsModel.ts
+++ infrastructure/models/SettingsModel.ts
@@ -18,7 +18,7 @@ const SettingsSchema: Schema = new Schema({
     }],
     referralMessage: {
         type: String,
-        default: '¡Hola {nombre_referido}! 👋 Tu amigo/a {nombre_comprador} nos pasó tu contacto porque sabe que quieres estrenar celular. Tenemos excelentes opciones para ti. 📱'
+        default: '¡Hola {nombre_referido}! 😊 ¿Cómo vas? Te escribo de parte de {nombre_comprador}, quien nos recomendó contigo. \n\nPasaba por aquí para poner a tu disposición nuestro catálogo, ya que contamos con gran variedad de marcas y modelos para entrega inmediata. 📱✨\n\nLo mejor de todo es que tenemos las mayores facilidades del mercado:\n✅ ¡Desde $0 de cuota inicial! (Aprobamos incluso si estás reportado o sin historial).\n✅ Créditos rápidos con Addi, Sistecredito, SuCupo y Krediya.\n✅ Compras de contado con precios especiales.\n\n¡Estrenar es más fácil de lo que crees! ¿Te gustaría que te enviara nuestro catálogo de hoy para que nos conozcas?'
     },
 }, { timestamps: true });
 
